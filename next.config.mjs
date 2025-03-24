b51658919fe322ccfda5964ad4ea2fea8296e195
Sanzadinternational <sanzadinternational5@gmail.com>
2025-03-24 17:05:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Disable strict mode
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
 };
 
 export default nextConfig;
