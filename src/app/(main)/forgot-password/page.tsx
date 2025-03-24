5d6b7d477f7153e6ba4eb2e0c6cf5fee12b7b506
Sanzadinternational <sanzadinternational5@gmail.com>
2025-03-24 17:10:52 +0530

    Update page.tsx

--- src/app/(main)/forgot-password/page.tsx
+++ src/app/(main)/forgot-password/page.tsx
@@ -4,7 +4,9 @@ const page = () => {
   return (
     <div className="flex flex-col justify-center items-center">
         <h1 className="text-3xl font-bold mb-3">Reset Password</h1>
+      <Suspense fallback={<div>Loading...</div>}>
         <ResetPassword/>
+        </Suspense>
     </div>
   )
 }
