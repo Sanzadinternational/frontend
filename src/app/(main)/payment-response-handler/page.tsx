87818b0d4aa7876e619072789d1ff1b8cf13eb7c
Sanzadinternational <sanzadinternational5@gmail.com>
2025-05-04 14:17:59 +0530

    Update page.tsx

--- src/app/(main)/payment-response-handler/page.tsx
+++ src/app/(main)/payment-response-handler/page.tsx
@@ -1,1 +1,13 @@
+import { Suspense } from 'react';
+import Login from '@/components/payment-response-handler';
 
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center my-8">
+      <Suspense fallback={<div>Loading...</div>}>
+            <payment-response-handler/>
+          </Suspense>
+     
+    </div>
+  );
+}
