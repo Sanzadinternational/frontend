0dcc733c7d66e658989e7914b1b0e60394a16aff
Sanzadinternational <sanzadinternational5@gmail.com>
2025-05-04 14:18:51 +0530

    Update page.tsx

--- src/app/(main)/payment-response-handler/page.tsx
+++ src/app/(main)/payment-response-handler/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="flex justify-center items-center my-8">
       <Suspense fallback={<div>Loading...</div>}>
-            <payment-response-handler/>
+            <PaymentResponseHandler/>
           </Suspense>
      
     </div>
